7594e09b385fe73db8c466f8f2d902ae2ca642ff
Ben Wells <benwellsmusic@gmail.com>
2014-01-18 21:44:15 -0500

    updated links for static pages index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Waxtracker::Application.routes.draw do
   devise_for :users
   resources :records
 
-  root "records#index"
+  root "static_pages#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
